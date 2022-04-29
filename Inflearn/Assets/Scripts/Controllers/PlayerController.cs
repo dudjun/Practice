a3c2f4eeab65d3e7f3f8444f37a3ca43a6b26014
dudjun <dud4817@naver.com>
2022-04-30 01:05:41 +0900

    2022 04 30 Inflearn
    2022 04 30 Inflearn

--- Inflearn/Assets/Scripts/Controllers/PlayerController.cs
+++ Inflearn/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,16 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    float _speed = 10.0f;
-
+	PlayerStat _stat;
 	Vector3 _destPos;
 
     void Start()
     {
+		_stat = gameObject.GetComponent<PlayerStat>();
+
 		Managers.Input.MouseAction -= OnMouseClicked;
 		Managers.Input.MouseAction += OnMouseClicked;		
 	}
@@ -20,6 +21,7 @@ public class PlayerController : MonoBehaviour
 		Die,
 		Moving,
 		Idle,
+		Skill,
 	}
 
 	PlayerState _state = PlayerState.Idle;
@@ -33,21 +35,33 @@ public class PlayerController : MonoBehaviour
 	void UpdateMoving()
 	{
 		Vector3 dir = _destPos - transform.position;
-		if (dir.magnitude < 0.0001f)
+		if (dir.magnitude < 0.1f)
 		{
 			_state = PlayerState.Idle;
 		}
 		else
 		{
-			float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
-			transform.position += dir.normalized * moveDist;
+			NavMeshAgent nma = gameObject.GetOrAddComponent<NavMeshAgent>();
+
+			float moveDist = Mathf.Clamp(_stat.MoveSpeed * Time.deltaTime, 0, dir.magnitude);
+			// nma.CalculatePath
+			nma.Move(dir.normalized * moveDist);
+
+			Debug.DrawRay(transform.position + Vector3.up * 0.5f, dir.normalized, Color.green);
+			if (Physics.Raycast(transform.position + Vector3.up * 0.5f, dir, 1.0f, LayerMask.GetMask("Block")))
+            {
+				_state = PlayerState.Idle;
+				return;
+            }
+
+			//transform.position += dir.normalized * moveDist;
 			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 20 * Time.deltaTime);
 		}
 
 		// 애니메이션
 		Animator anim = GetComponent<Animator>();
 		// 현재 게임 상태에 대한 정보를 넘겨준다
-		anim.SetFloat("speed", _speed);
+		anim.SetFloat("speed", _stat.MoveSpeed);
 	}
 
 	void UpdateIdle()
@@ -74,19 +88,29 @@ public class PlayerController : MonoBehaviour
 		}
 	}
 
+	int _mask = (1 << (int)Define.Layer.Ground) | (1 << (int)Define.Layer.Monster);
 	void OnMouseClicked(Define.MouseEvent evt)
 	{
 		if (_state == PlayerState.Die)
 			return;
 
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+		//Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
 
 		RaycastHit hit;
-		if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
+		if (Physics.Raycast(ray, out hit, 100.0f, _mask))
 		{
 			_destPos = hit.point;
 			_state = PlayerState.Moving;
+
+			if (hit.collider.gameObject.layer == (int)Define.Layer.Monster)
+            {
+				Debug.Log("Monster Click");
+            }
+            else
+            {
+				Debug.Log("Ground Click");
+			}
 		}
 	}
 }
